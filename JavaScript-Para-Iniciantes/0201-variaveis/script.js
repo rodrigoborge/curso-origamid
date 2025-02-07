46591304da4abc4a48777cd006cf5bf0f650caa4
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-07 20:22:27 -0300

    Update script.js

---
+++ JavaScript-Para-Iniciantes/0201-variaveis/script.js
@@ -0,0 +1,21 @@
+//var ->
+var nome = 'Rodrigo';
+var idade = 35;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Borge',
+    cidade = 'Curitiba',
+    estado = 'Paraná';
+
+console.log(cidade);
+
+var semDefinir;
+console.log(semDefinir);
