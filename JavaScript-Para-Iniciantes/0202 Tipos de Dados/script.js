bb1d96c2cab056b18615b0569d6cdc865c822cbe
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-08 00:24:24 -0300

    Update script.js

---
+++ JavaScript-Para-Iniciantes/0202 Tipos de Dados/script.js
@@ -0,0 +1,39 @@
+var nome = 'Rodrigo';
+var idade = 35;
+var time = null;
+var simbolo = Symbol();
+
+console.log(typeof simbolo);
+
+var nome = 'Rodrigo';
+var sobrenome = 'Borge';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = `Romário fez ${gols} gols`;
+
+console.log(frase);
+
+// Declare uma variável contendo uma string
+var nome = 'Rodrigo';
+
+// Declare uma variável contendo um número dentro de uma string
+var numeroString = '30';
+
+// Declare uma variável com a sua idade
+var idade = 35;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Rodrigo',
+    sobrenome = 'Borge';
+console.log(nome + ' ' + sobrenome);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase2 = "It's time";
+console.log(frase2);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
