4e8ba9156b2db218485bb3cfbf79e250e405a777
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-08 20:50:09 -0300

    Update script.js

--- JavaScript-Para-Iniciantes/0204-Boolean e Condicionais 1/script.js
+++ JavaScript-Para-Iniciantes/0204-Boolean e Condicionais 1/script.js
@@ -6,5 +6,23 @@ if (possuiGraduacao) {
 } else if (possuiDoutorado) {
     console.log('É falso');
 } else {
-    console.log('Não possui nada')
+    console.log('Não possui nada');
 }
+
+
+// Valor Lógico E - &&
+var condicional = (5 - 5) && (5 + 5);
+
+if (condicional) {
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso');
+}
+
+if ((5 - 10) && (5 + 5)) {
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso');
+}
+
+// Valor Lógico OU - ||
