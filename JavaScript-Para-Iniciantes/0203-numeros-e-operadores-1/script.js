5208615eea62fa6443fe07f2dbbcd44223fbe89f
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-08 00:34:28 -0300

    numeros e operadores

---
+++ JavaScript-Para-Iniciantes/0203-numeros-e-operadores-1/script.js
@@ -0,0 +1,8 @@
+//Operadores Aritiméticos
+var soma = 100 + 50;
+var subtracao = 100 - 50;
+var multiplicacao = 100 *2;
+var divisao = 100 / 2;
+var expoente = 2 ** 4;
+var modulo = 14 % 5; // -> Resto da divisão
+
