90e256b403ca63b77be8603b56ace3ce72f64636
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-09 18:36:24 -0300

    Update script.js

--- JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
+++ JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
@@ -1,2 +1,10 @@
 var videoGames = ['Switch', 'PS4', 'XBox'];
+console.log(videoGames);
 console.log(videoGames[2]);
+
+videoGames.pop(); // -> .pop remove o ultimo item do array
+console.log(videoGames);
+
+videoGames.push('3DS'); // -> .push adiciona um item no final do array
+console.log(videoGames);
+
