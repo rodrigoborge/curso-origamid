279db2d9f62a0b985fb626a96875b7d0cc761dbf
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-09 18:48:13 -0300

    Update script.js

--- JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
+++ JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
@@ -1,4 +1,4 @@
-var videoGames = ['Switch', 'PS4', 'XBox'];
+var videoGames = ['Switch', 'PS4', 'XBox', '3DS'];
 console.log(videoGames);
 console.log(videoGames[2]);
 
@@ -8,14 +8,26 @@ console.log(videoGames);
 videoGames.push('3DS'); // -> .push adiciona um item no final do array
 console.log(videoGames);
 
+console.log('________________________');
+
 // LOOPS e FOR
-for (var numero = 0; numero <= 10; numero++) {
+for (var numero = 0; numero <= 2; numero++) {
     console.log('Eu sou o for: ' + numero);
 }
 
+console.log('________________________');
+
 // LOOPS WHILE
 var i = 0;
-while (i <= 10) {
+while (i <= 2) {
     console.log('Eu sou o while: ' + i);
     i++;
+}
+
+console.log('________________________');
+
+var videoGames2 = ['Switch', 'PS4', 'XBox', '3DS'];
+
+for (var item = 0; item < videoGames2.length; item++) {
+    console.log(videoGames2[item]);
 }
