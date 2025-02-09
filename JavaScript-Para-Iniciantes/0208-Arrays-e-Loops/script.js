20e08b3a3331cdec2d800dd133c268af00ece10b
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-09 18:56:58 -0300

    Update script.js

--- JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
+++ JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
@@ -30,4 +30,16 @@ var videoGames2 = ['Switch', 'PS4', 'XBox', '3DS'];
 
 for (var item = 0; item < videoGames2.length; item++) {
     console.log(videoGames2[item]);
+    if (videoGames2[item] === 'XBox') {
+        break;
+    }
 }
+
+console.log('________________________');
+
+// -> Essa é a maneira mais simples e indicada para fazer iteraçao no JavaScript
+var frutas = ['Banana', 'Maçã', 'Uva', 'Morango', 'Mamão'];
+
+frutas.forEach(function (item, index, array) {
+    console.log(item, index, array);
+});
