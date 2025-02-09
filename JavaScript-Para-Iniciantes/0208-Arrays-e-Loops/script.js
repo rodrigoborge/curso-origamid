b7405a8d819a5723bdda8eb20c3464d591506632
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-09 19:18:50 -0300

    Update script.js

--- JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
+++ JavaScript-Para-Iniciantes/0208-Arrays-e-Loops/script.js
@@ -43,3 +43,32 @@ var frutas = ['Banana', 'Maçã', 'Uva', 'Morango', 'Mamão'];
 frutas.forEach(function (item, index, array) {
     console.log(item, index, array);
 });
+
+console.log('------------ EXERCICIOS ------------');
+
+//EXERCICIOS
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var brasilCampeao = [1959, 1962, 1970, 1994, 2002];
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+brasilCampeao.forEach(function (item) {
+    console.log(`O brasil ganhou a copa de ${item}`);
+});
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância'];
+console.log(frutas);
+for (i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+    if (frutas[i] === 'Pera') {
+        break;
+    }
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+
+var ultimaFruta = frutas[frutas.length-1];
+console.log(`A ultima fruta do array é ${ultimaFruta}`);
