2ef7a9795d2389d8c2ff81d967bf64ab79311cdc
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-10 00:41:09 -0300

    Atribuição e Ternário

---
+++ JavaScript-Para-Iniciantes/0209-Atribuicao-e-Ternario/script.js
@@ -0,0 +1,14 @@
+// Operadores de Atribuiçao
+
+// Podem funcionar como formas abreviadas
+
+//Exemplos
+var x = 5;
+var y = 10;
+
+x += y; // -> x = x + y = 15
+x -= y; // -> x = x - y = -5
+x *= y; // -> x = x * y = 50
+x /= y; // -> x = x / y = 0.5
+x %= y; // -> x = x % y = 0
+x **= y; // -> x = x ** y = 9765625
