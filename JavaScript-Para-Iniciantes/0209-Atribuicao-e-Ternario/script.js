5a54a5206d0b68e3be8dce0190c5423b50ed3520
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-10 01:04:10 -0300

    Update script.js

--- JavaScript-Para-Iniciantes/0209-Atribuicao-e-Ternario/script.js
+++ JavaScript-Para-Iniciantes/0209-Atribuicao-e-Ternario/script.js
@@ -26,6 +26,8 @@ console.log(podeBeber);
 // Some 500 ao valor de scroll abaixo,
 // atribuindo o novo valor a scroll
 var scroll = 1000;
+scroll += 500;
+console.log(scroll);
 
 // Atribua true para a variável darCredito,
 // caso o cliente possua carro e casa.
@@ -34,11 +36,5 @@ var possuiCarro = true;
 var possuiCasa = true;
 var darCredito;
 
-
-// Fiz com o IF padrão
-if (possuiCarro === true && possuiCasa === true) {
-    darCredito = true;
-} else {
-    darCredito = false;
-}
+darCredito = possuiCarro && possuiCasa;
 console.log(darCredito);
