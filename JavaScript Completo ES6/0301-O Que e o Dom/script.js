beeb7a5f92087042f8c6e976a4b3555983c7bb85
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-10 21:44:52 -0300

    Update script.js

--- JavaScript Completo ES6/0301-O Que e o Dom/script.js
+++ JavaScript Completo ES6/0301-O Que e o Dom/script.js
@@ -15,6 +15,22 @@ h1Selecionado.addEventListener('click', function () {
     console.log('Clicou em', h1Selecionado.innerText);
 });
 
-
 // EXERCICIOS
 
+// Retorne o url da página atual utilizando o objeto window
+const urlAtual = window.location.href;
+console.log(`Este é a URL que você está: ${urlAtual}`);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const selecionaElemento = document.querySelector('.titulo-principal');
+console.log(selecionaElemento);
+
+// Retorne a linguagem do navegador
+const linguagemNavegador = navigator.language;
+console.log(`Esta é o idioma do navegador ${linguagemNavegador}`);
+
+// Retorne a largura da janela
+const larguraJanela = window.innerWidth;
+const alturaJanela = window.innerHeight;
+console.log(`Esta é a largura da janela ${larguraJanela} x ${alturaJanela} pixels`);
