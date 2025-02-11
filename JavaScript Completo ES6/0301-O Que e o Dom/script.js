c59fd3b563414d3fd053b9a624171f84ecc77789
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-10 21:32:13 -0300

    node

--- JavaScript Completo ES6/0301-O Que e o Dom/script.js
+++ JavaScript Completo ES6/0301-O Que e o Dom/script.js
@@ -4,3 +4,17 @@ console.log(href);
 const h1Selecionado = document.querySelector('h1');
 
 // NODE
+
+const h1Classes = h1Selecionado.classList;
+
+function callBackh1() {
+    console.log('Clicou em', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', function () {
+    console.log('Clicou em', h1Selecionado.innerText);
+});
+
+
+// EXERCICIOS
+
