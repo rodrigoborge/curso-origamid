dee023fbf37b75b20174ad06954ba3f57a920fda
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-12 19:36:35 -0300

    Update script.js

--- JavaScript Completo ES6/0304-forEach-e-Arrow-Function/script.js
+++ JavaScript Completo ES6/0304-forEach-e-Arrow-Function/script.js
@@ -7,20 +7,41 @@
 // });
 
 // Transformar em Array
-const titulos = document.getElementsByClassName('titulo');
-const titulosArray = Array.from(titulos);
+// const titulos = document.getElementsByClassName('titulo');
+// const titulosArray = Array.from(titulos);
 
-titulosArray.forEach(function (item) {
-    console.log(item);
-});
+// titulosArray.forEach(function (item) {
+//     console.log(item);
+// });
 
 // Arrow Function
+// const imgs = document.querySelectorAll('img');
+
+// let iu = 0;
+// imgs.forEach((item) => {
+//     console.log(iu++);
+// });
+
+// let i = 0;
+// imgs.forEach(() => console.log(i++));
+
+// EXERCICIOS
+// Mostre no console cada parágrado do site
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos);
+
+// Mostre o texto dos parágrafos no console
+const paragrafosTag = document.getElementsByTagName('p');
+console.log(paragrafosTag.innerText);
+
+// Como corrigir os erros abaixo:
 const imgs = document.querySelectorAll('img');
 
-let iu = 0;
-imgs.forEach((item) => {
-    console.log(iu++);
+imgs.forEach((item, index) => {
+    console.log(item, index);
 });
 
 let i = 0;
 imgs.forEach(() => console.log(i++));
+
+imgs.forEach(() => i++);
