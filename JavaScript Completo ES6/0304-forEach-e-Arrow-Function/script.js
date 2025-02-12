de66eaade471bb20fbcac0535b374ef17e597674
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-12 19:16:42 -0300

    Update script.js

--- JavaScript Completo ES6/0304-forEach-e-Arrow-Function/script.js
+++ JavaScript Completo ES6/0304-forEach-e-Arrow-Function/script.js
@@ -2,7 +2,14 @@ const imgs = document.querySelectorAll('img');
 console.log(imgs);
 
 let i = 0;
-imgs.forEach(function (item, index,array) {
+imgs.forEach(function (item, index, array) {
     // console.log(item, index, array);
 });
 
+// Transformar em Array
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+titulosArray.forEach(function (item) {
+    console.log(item);
+});
