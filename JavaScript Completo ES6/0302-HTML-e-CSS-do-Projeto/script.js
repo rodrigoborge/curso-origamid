996e2bd1d1035a818db0c4b9573a8188c43c7dd0
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-11 23:56:45 -0300

    Update script.js

---
+++ JavaScript Completo ES6/0302-HTML-e-CSS-do-Projeto/script.js
@@ -0,0 +1,16 @@
+const animais = document.getElementById('animais');
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi)
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linksInternos = document.querySelector('[href^="#"]');
+console.log(linksInternos);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[3])
