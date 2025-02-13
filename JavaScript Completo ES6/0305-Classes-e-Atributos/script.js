75ca1dbe250b5f0311c9fc19dfd7a9a0a94139ef
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-12 23:17:32 -0300

    Update script.js

---
+++ JavaScript Completo ES6/0305-Classes-e-Atributos/script.js
@@ -0,0 +1,22 @@
+// Manipulando Classes
+const menu = document.querySelector('.menu');
+menu.classList.add('ativo');
+menu.classList.remove('azul');
+menu.classList.toggle('azul');
+
+if (menu.classList.contains('azul')) {
+    menu.classList.add('possuiAzul');
+} else {
+    menu.classList.add('naoPossuiAzul');
+}
+
+menu.className += ' Vermelho';
+
+console.log(menu.className);
+console.log(menu.classList);
+
+// Attributes
+const animais = document.querySelector('.animais');
+console.log(animais.attributes['data-texto']);
+
+// GETAttribute e SETAttribute
