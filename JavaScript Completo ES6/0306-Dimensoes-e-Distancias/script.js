75eb92a7c8ee71276552723e05d965a356a10674
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-13 23:24:01 -0300

    Update script.js

--- JavaScript Completo ES6/0306-Dimensoes-e-Distancias/script.js
+++ JavaScript Completo ES6/0306-Dimensoes-e-Distancias/script.js
@@ -8,3 +8,6 @@ console.log(animaisTop);
 const primeiroh2 = document.querySelector('h2');
 const h2left = primeiroh2.offsetLeft;
 console.log(h2left);
+
+const rect = primeiroh2.getBoundingClientRect();
+console.log(rect);
