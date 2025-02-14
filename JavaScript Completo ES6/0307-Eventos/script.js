af50f31b4f9e8788e36c5fe854e93394196bc4c8
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-14 20:53:24 -0300

    Update script.js

--- JavaScript Completo ES6/0307-Eventos/script.js
+++ JavaScript Completo ES6/0307-Eventos/script.js
@@ -29,10 +29,18 @@ function handleEvent(event) {
     console.log(event.type, event);
 }
 
-h1.addEventListener('click', handleEvent);
-h1.addEventListener('mouseenter', handleEvent);
-h1.addEventListener('mousemove', handleEvent);
-h1.addEventListener('mousemove', handleEvent);
-window.addEventListener('scroll', handleEvent);
-window.addEventListener('resize', handleEvent);
-window.addEventListener('keydown', handleEvent);
+// h1.addEventListener('click', handleEvent);
+// h1.addEventListener('mouseenter', handleEvent);
+// h1.addEventListener('mousemove', handleEvent);
+// h1.addEventListener('mousemove', handleEvent);
+// window.addEventListener('scroll', handleEvent);
+// window.addEventListener('resize', handleEvent);
+function handleKeyboard(event) {
+    if (event.key === 'f') {
+        document.body.classList.toggle('fullscreen');
+    } else if (event.key === 'g') {
+        document.body.classList.remove('fullscreen');
+    }
+}
+
+window.addEventListener('keydown', handleKeyboard);
