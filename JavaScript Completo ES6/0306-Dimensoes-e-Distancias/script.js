d684495fb8c65dd83b737bf2df8fa8affd1d5840
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-13 23:20:55 -0300

    Create script.js

---
+++ JavaScript Completo ES6/0306-Dimensoes-e-Distancias/script.js
@@ -0,0 +1,10 @@
+const listaAnimais = document.querySelector('.animais-lista');
+const height = listaAnimais.scrollHeight;
+console.log(height);
+
+const animaisTop = listaAnimais.offsetTop;
+console.log(animaisTop);
+
+const primeiroh2 = document.querySelector('h2');
+const h2left = primeiroh2.offsetLeft;
+console.log(h2left);
