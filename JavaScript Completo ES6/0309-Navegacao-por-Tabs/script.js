455f50c55a3bcb6923663fcc98457b0ec08a9970
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-15 20:09:06 -0300

    Update script.js

--- JavaScript Completo ES6/0309-Navegacao-por-Tabs/script.js
+++ JavaScript Completo ES6/0309-Navegacao-por-Tabs/script.js
@@ -3,13 +3,13 @@ const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 function activeTab(index) {
     tabContent.forEach((section) => {
-        section.classList.remove('ativa');
+        section.classList.remove('ativo');
     });
     tabContent[index].classList.add('ativo');
 }
 
 tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', function () {
+    itemMenu.addEventListener('click', () => {
         activeTab(index);
     });
 });
