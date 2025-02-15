7bca8d0b40f83be95bea9f90fd8e03e377284acc
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-14 22:44:10 -0300

    Update script.js

--- JavaScript Completo ES6/0308-Traversing-e-Manipulacao/script.js
+++ JavaScript Completo ES6/0308-Traversing-e-Manipulacao/script.js
@@ -27,3 +27,17 @@ const novoH1 = document.createElement('h1');
 novoH1.innerText = 'Novo Título';
 novoH1.classList.add('titulo');
 contato.appendChild(novoH1);
+
+// EXERCICIOS
+
+// Duplique o menu e adicione ele em copy
+const menu = document.querySelector('.menu');
+const copy = document.querySelector('.copy');
+const cloneMenu = menu.cloneNode(true);
+copy.appendChild(cloneMenu);
+
+// Selecione o primeiro DT da dl de Faq
+
+// Selecione o DD referente ao primeiro DT
+
+// Substitua o conteúdo html de .faq pelo de .animais
