c844bc2033899605ca22514b926a4654496e07ce
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-14 22:18:14 -0300

    Update script.js

---
+++ JavaScript Completo ES6/0308-Traversing-e-Manipulacao/script.js
@@ -0,0 +1,12 @@
+// Traversing e Manipulação
+
+// innerHTML
+const h1 = document.querySelector('h1');
+console.log(h1.innerHTML);
+
+const animaisLista = document.querySelector('.animais-lista');
+console.log(animaisLista.innerHTML);
+
+// innerText
+const animaisDescricao = document.querySelector('.animais-descricao');
+console.log(animaisDescricao.innerText);
