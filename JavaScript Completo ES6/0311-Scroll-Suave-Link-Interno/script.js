9180baf3e47c6eacc4a8dae680702c2f1ceb30fa
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-15 22:33:44 -0300

    Scroll Suave Link Interno

--- JavaScript Completo ES6/0311-Scroll-Suave-Link-Interno/script.js
+++ JavaScript Completo ES6/0311-Scroll-Suave-Link-Interno/script.js
@@ -40,3 +40,15 @@ const activeClass = 'ativo'
     }
 }
 initiAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+
+function scrollToSection(event){
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection)
+})
