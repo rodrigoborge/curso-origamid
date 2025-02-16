b20fd7ad5a1a617a9fa4a52b7123505266047859
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-15 22:35:53 -0300

    Update script.js

--- JavaScript Completo ES6/0311-Scroll-Suave-Link-Interno/script.js
+++ JavaScript Completo ES6/0311-Scroll-Suave-Link-Interno/script.js
@@ -23,7 +23,7 @@ initTabNav();
 
 function initiAccordion() {
     const accordionList = document.querySelectorAll('.js-accordion dt');
-const activeClass = 'ativo'
+    const activeClass = 'ativo';
 
     if (accordionList.length) {
         accordionList[0].classList.add(activeClass);
@@ -41,14 +41,18 @@ const activeClass = 'ativo'
 }
 initiAccordion();
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event){
+function scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
 }
 
 linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection)
-})
+    link.addEventListener('click', scrollToSection);
+});
