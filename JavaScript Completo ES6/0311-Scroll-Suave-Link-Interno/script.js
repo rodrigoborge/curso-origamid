87a839818a3dc8de6068062258ad8c817a51ec38
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-15 22:37:13 -0300

    Update script.js

--- JavaScript Completo ES6/0311-Scroll-Suave-Link-Interno/script.js
+++ JavaScript Completo ES6/0311-Scroll-Suave-Link-Interno/script.js
@@ -41,18 +41,21 @@ function initiAccordion() {
 }
 initiAccordion();
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
     });
 }
-
-linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-});
+initScrollSuave();
