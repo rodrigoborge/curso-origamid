a91b5272062581a19e978b6d3b3a416231bee8f0
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-15 22:45:40 -0300

    Update script.js

--- JavaScript Completo ES6/0312-Animacao-ao-Scroll/script.js
+++ JavaScript Completo ES6/0312-Animacao-ao-Scroll/script.js
@@ -60,3 +60,15 @@ function initScrollSuave() {
 }
 initScrollSuave();
 
+const sections = document.querySelectorAll('.js-scroll');
+
+function animaScroll() {
+    sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect();
+        if (sectionTop < 0) {
+            section.classList.add('ativo');
+        }
+    });
+}
+
+window.addEventListener('scroll', animaScroll);
