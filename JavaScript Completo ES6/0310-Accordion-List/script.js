f5d66f2c825d34c18148edc871c1744d36dd191e
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-15 22:05:52 -0300

    Update script.js

--- JavaScript Completo ES6/0310-Accordion-List/script.js
+++ JavaScript Completo ES6/0310-Accordion-List/script.js
@@ -20,3 +20,14 @@ function initTabNav() {
     }
 }
 initTabNav();
+
+const accordionList = document.querySelectorAll('js-accordion dt');
+
+function activeAccordion() {
+    this.classList.add('ativo');
+    this.nextElementSibling.classList.add('ativo');
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+});
