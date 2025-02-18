732ba4a5f44bb7e6da9dd20305fb504188a7ffc6
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-17 21:30:41 -0300

    Update script.js

--- JavaScript Completo ES6/0401-Constructor-Function/script.js
+++ JavaScript Completo ES6/0401-Constructor-Function/script.js
@@ -1,14 +1,28 @@
-const Dom = {
-    seletor: 'li',
-    element() {
-        return document.querySelector('this.seletor');
+// const Dom = {
+//     seletor: 'li',
+//     element() {
+//         return document.querySelector('this.seletor');
+//     },
+
+//     ativar() {
+//         this.element().classList.add('ativar');
+//     },
+// };
+
+// Dom.ativar();
+// Dom.seletor = 'ul';
+// Dom.ativar();
+
+
+
+// FUNCAO SELETORA
+function Dom(seletor) {
+    this.element = function () {
+        return document.querySelector(seletor);
     },
 
-    ativar() {
+    this.ativar = function () {
         this.element().classList.add('ativar');
-    },
+    }
 };
 
-Dom.ativar();
-Dom.seletor = 'ul';
-Dom.ativar();
