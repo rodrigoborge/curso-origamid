1a378f42ba112a991e659b774882d8d41777088d
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-20 18:20:24 -0300

    Prototype

--- JavaScript Completo ES6/0402-Prototype/script.js
+++ JavaScript Completo ES6/0402-Prototype/script.js
@@ -1,23 +1,22 @@
-function Pessoa(nome,idade){
-    this.nome
-    this.idade
-    this.abracar = function(){
-        return 'Abracou'
-    }
-    this.andar = function (){
-        return 'Andou pelo objeto!'
-    }
+function Pessoa(nome, idade) {
+    this.nome;
+    this.idade;
+    this.abracar = function () {
+        return 'Abracou';
+    };
+    this.andar = function () {
+        return 'Andou pelo objeto!';
+    };
 }
 
-Pessoa.prototype.andar = function(){
-    return 'Pessoa andou'
-}
+Pessoa.prototype.andar = function () {
+    return 'Pessoa andou';
+};
 
+Pessoa.prototype.nadar = function () {
+    return 'Pessoa nadou';
+};
 
-Pessoa.prototype.nadar = function(){
-    return 'Pessoa nadou'
-}
+const andre = new Pessoa('André', 28);
 
-const andre = new Pessoa('André', 28)
-
-console.log(Pessoa.prototype)
+console.log(Pessoa.prototype);
