ab1b32bc7c51ef120f4577fe6e873906b0c34a8b
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-19 21:11:08 -0300

    Update script.js

--- JavaScript Completo ES6/0402-Prototype/script.js
+++ JavaScript Completo ES6/0402-Prototype/script.js
@@ -1,3 +1,12 @@
-function teste(){
-    return 1+1;
+function Pessoa(nome,idade){
+    this.nome
+    this.idade
 }
+
+Pessoa.prototype.andar = function(){
+    return 'Pessoa andou'
+}
+
+const andre = new Pessoa('André', 28)
+
+console.log(Pessoa.prototype)
