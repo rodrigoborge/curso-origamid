d08d7637de6be206ba7d286a5cfc8cc19975608f
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-25 23:26:40 -0300

    Update script.js

--- JavaScript Completo ES6/0403-Native-Host-e-User/script.js
+++ JavaScript Completo ES6/0403-Native-Host-e-User/script.js
@@ -7,3 +7,10 @@ String
 Array
 Function
 
+//Host
+//São implementados pelo próprio ambiente, no caso, o navegador, como exemplo o DomList, HTMLCollection e outros. Em Node.js os objetos do host são diferentes, já que não temos o ambiente do browser
+
+//Objetos do Browser
+NodeList
+HTMLCollection
+Element
