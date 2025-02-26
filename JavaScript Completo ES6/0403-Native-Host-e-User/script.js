25cfc327fe7c7832bf734859d0426aa5da44faab
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-25 23:28:25 -0300

    Update script.js

--- JavaScript Completo ES6/0403-Native-Host-e-User/script.js
+++ JavaScript Completo ES6/0403-Native-Host-e-User/script.js
@@ -2,15 +2,23 @@
 //São aqueles definidos na especificaçao da linguagem e são implementados independente do host
 
 //Exemplos:
-Object
-String
-Array
-Function
+Object;
+String;
+Array;
+Function;
 
 //Host
 //São implementados pelo próprio ambiente, no caso, o navegador, como exemplo o DomList, HTMLCollection e outros. Em Node.js os objetos do host são diferentes, já que não temos o ambiente do browser
 
 //Objetos do Browser
-NodeList
-HTMLCollection
-Element
+NodeList;
+HTMLCollection;
+Element;
+
+//User
+//São os objetos definidos pelo seu aplicativo. Ou seja, qualquer objeto que você criar ou que importar de alguma biblioteca externa
+
+//Exemplo
+const Pessoa = {
+    nome: 'Rodrigo',
+};
