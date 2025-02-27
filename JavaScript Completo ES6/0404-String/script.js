142b18c7d272919a229274f326eeee5218296669
Rodrigo Borge <rodrigo@borge.com.br>
2025-02-27 00:00:55 -0300

    Update script.js

--- JavaScript Completo ES6/0404-String/script.js
+++ JavaScript Completo ES6/0404-String/script.js
@@ -1,2 +1,5 @@
 //Replace
-//Troca parte da string por outra. Podemos utilizar uma regular expression ou um valor direto. Se
+//Troca parte da string por outra. Podemos utilizar uma regular expression ou um valor direto. Se usarmos um valor direto ele irá trocar apenas o primeiro valor que encontrar.
+
+let listaItens = 'Camisas Bonés Calças Bermudas Vestidos Saias';
+listaItens = listaItens.replace(/[ ]+/g, ', ');
