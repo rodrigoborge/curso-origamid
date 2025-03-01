d2dc488ad2f52136bbee0c3ef7194c1b153c05bf
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-01 02:36:44 -0300

    Update script.js

--- JavaScript Completo ES6/0405-Number-e-Math/script.js
+++ JavaScript Completo ES6/0405-Number-e-Math/script.js
@@ -29,3 +29,16 @@ console.log(numeroAleatorio);
 
 let numeroAleatorio2 = Math.floor(Math.random() * (60 - 1 + 1)) + 1;
 console.log(numeroAleatorio2);
+
+// EXERCICIOS
+
+// Retorne um número aleatório
+// entre 1050 e 2000
+
+// Retorne o maior número da lista abaixo
+const numeros = '4, 5, 20, 8, 9';
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222', 'R$ 230  ', 'r$  200'];
