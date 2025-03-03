beb7e19c530063f03644b34c58aa1f940b78637b
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-02 23:33:05 -0300

    Update script.js

---
+++ JavaScript Completo ES6/0406-Array/script.js
@@ -0,0 +1,17 @@
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const precos = [49, 99, 69, 89];
+
+const dados = [
+    new String('Tipo 1'),
+    ['Carro', 'Portas', { cor: 'Azul', preco: 2000 }],
+    function andar(nome) {
+        console.log(nome);
+    },
+];
+
+const carros = new Array('Ford', 'Fiat', 'Honda');
+
+carros[2] = 'Ferrari';
+carros[3] = 'Kia';
+carros[20] = 'BYD';
+console.log(carros.length);
