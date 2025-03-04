40ff6ecd3e2e1ea76c4db94b548ebeac5a111961
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-04 00:07:01 -0300

    Update script.js

--- JavaScript Completo ES6/0406-Array/script.js
+++ JavaScript Completo ES6/0406-Array/script.js
@@ -80,3 +80,10 @@ console.log(['Item1', 'Item2', 'Item3', 'Item4', 'Item5'].copyWithin(-1));
 //Métodos de Acesso [].concat()
 //Os métodos abaixo não modificam a array original, apenas retornam uma array modificada. [].concat() irá concatenar a array com o valor passado
 
+const transporte1 = ['Barco', 'Aviao'];
+const transporte2 = ['Carro', 'Moto'];
+const transportes = transporte1.concat(transporte2);
+// ['Barco', 'Aviao', 'Carro', 'Moto'];
+
+const maisTransportes = [].concat(transporte1, transporte2, 'Van');
+// ['Barco', 'Aviao', 'Carro', 'Moto', 'Van'];
