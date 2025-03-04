35193612c8056287cbed0fd159e78a16dc6dcf45
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-04 00:00:47 -0300

    Update script.js

--- JavaScript Completo ES6/0406-Array/script.js
+++ JavaScript Completo ES6/0406-Array/script.js
@@ -60,4 +60,4 @@ console.log(carros);
 //[].copyWithin()
 //[]/copyWithin(alvo, inicio, final) a partir do alvo, ele irá copiar a array começando do inicio até o final e vai preencher a mesma com essa cópia. Caso omita os valores de início e final, ele irá utilizar como inicio o 0 e final o valor total da array.
 
-['Item1', 'Item2', 'Item3', 'Item4'].copyWithin(2, 0, 3);
+console.log(['Item1', 'Item2', 'Item3', 'Item4', 'Item5'].copyWithin(2, 0, 3));
