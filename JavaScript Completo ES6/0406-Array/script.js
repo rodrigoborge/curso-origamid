daf03ead14360de69501de87cd1cf81b741f3376
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-04 00:11:50 -0300

    Update script.js

--- JavaScript Completo ES6/0406-Array/script.js
+++ JavaScript Completo ES6/0406-Array/script.js
@@ -90,3 +90,11 @@ const maisTransportes = [].concat(transporte1, transporte2, 'Van');
 
 //[].includes(), [].indexOf() e [].lastIndexOf()
 //[].includes(valor) verifica se a array possui o valor e retorna true ou false. [].indexOf(valor) verifica se a array possui o valor e retorna o index do primeiro valor na array. Já o [].lastIndexOf(valor) retorna o index do último.
+
+const linguagens = ['html', 'css', 'js', 'php', 'python', 'js'];
+
+linguagens.includes('css'); // true
+linguagens.includes('ruby'); // false
+linguagens.indexOf('python'); // 4
+linguagens.indexOf('js'); // 2
+linguagens.lastIndexOf('js'); // 5
