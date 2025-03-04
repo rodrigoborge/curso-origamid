a4be59d424857842d8a4ab4a4b0e05bcb3c2b175
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-03 23:52:34 -0300

    Update script.js

--- JavaScript Completo ES6/0406-Array/script.js
+++ JavaScript Completo ES6/0406-Array/script.js
@@ -46,3 +46,9 @@ console.log(instrumentos);
 console.log(idades);
 
 //Splice
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+carros.splice(1, 0, 'Kia', 'Mustang'); // []
+carros; // ['Ford', 'Kia', 'Mustang', 'Fiat', 'VW', 'Honda']
+
+carros.splice(3, 2, 'Ferrari'); // ['Fiat', 'VW']
+carros; // ['Ford', 'Kia', 'Mustang', 'Ferrari', 'Honda']
