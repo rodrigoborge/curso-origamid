fcb0ef27469a1a4da544fa0533b5628f9ab3bd5f
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-05 20:34:56 -0300

    Update script.js

--- JavaScript Completo ES6/0407-Array-e-Iteracao/script.js
+++ JavaScript Completo ES6/0407-Array-e-Iteracao/script.js
@@ -1,3 +1,10 @@
 //[].forEach()
 //[].forEach(callback(itemAtual, index, array)) a função de callback é executada para cada item da array. Ela possui tres argumentos, itemAtual (valor do item da array), index (index do valor na array) e array (array original)
 
+const carros = ['Ford', 'Fiat', 'Honda'];
+carros.forEach((item, index, array) => {
+    console.log(item.toLocaleUpperCase(), index, array);
+});
+
+//Arrow Function
+
