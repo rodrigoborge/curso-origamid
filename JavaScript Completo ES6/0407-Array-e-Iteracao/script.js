c55dbd17a6a7459e9b8abb46b7470fc558ade5bf
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-05 23:11:27 -0300

    Update script.js

--- JavaScript Completo ES6/0407-Array-e-Iteracao/script.js
+++ JavaScript Completo ES6/0407-Array-e-Iteracao/script.js
@@ -166,3 +166,10 @@ const listaAulas = aulas.reduce((acumulador, atual, index) => {
 //[].reduceRight()
 //Existe o metodo [].reduceRight(), a diferença é que este começa a iterar da direita para a esquerda, enquanto o reduce itera da esquerda para a direita
 
+const frutas = ['Banana', 'Pêra', 'Uva'];
+
+const frutasRight = frutas.reduceRight((acc, fruta) => acc + ' ' + fruta);
+const frutasLeft = frutas.reduce((acc, fruta) => acc + ' ' + fruta);
+
+frutasRight; // Uva Pêra Banana
+frutasLeft; // Banana Pêra Uva
