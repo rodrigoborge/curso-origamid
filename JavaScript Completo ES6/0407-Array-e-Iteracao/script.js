b84011f57040e3bf94d03d0c61af36b9fc4515ac
Rodrigo Borge <rodrigo@borge.com.br>
2025-03-06 19:31:40 -0300

    Update script.js

--- JavaScript Completo ES6/0407-Array-e-Iteracao/script.js
+++ JavaScript Completo ES6/0407-Array-e-Iteracao/script.js
@@ -252,6 +252,20 @@ const aulasMaiores = aulas.filter((aula) => {
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
+const cursos = document.querySelectorAll('.cursos');
+const arrayCursos = Array.from(cursos);
+const objetoCursos = arrayCursos.map((curso) => {
+    const titulo = curso.querySelector('h1').innerText;
+    const descricao = curso.querySelector('p').innerText;
+    const aulas = curso.querySelector('.aulas').innerText;
+    const horas = curso.querySelector('.horas').innerText;
+    return {
+        titulo,
+        descricao,
+        aulas,
+        horas,
+    };
+});
 
 // Retorne uma lista com os
 // números maiores que 100
